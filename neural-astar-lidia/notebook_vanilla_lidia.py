7b75443bff24b56f0a7772e0c89efa7546a151f6
Lidia Fargueta Pelufo <at83yvah@lms41-28.e-technik.uni-erlangen.de>
2024-10-23 15:56:10 +0200

    Added images of CNN and CNN simple

--- neural-astar-lidia/notebook_vanilla_lidia.py
+++ neural-astar-lidia/notebook_vanilla_lidia.py
@@ -8,7 +8,7 @@ from src.neural_astar.utils.lidia_data import visualize_results
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-neural_astar = NeuralAstar(encoder_arch='CNN').to(device)
+neural_astar = NeuralAstar(encoder_arch='CNN', encoder_depth=4).to(device)
 neural_astar.load_state_dict(load_from_ptl_checkpoint("./model/mazes_032_moore_c8/lightning_logs/"))
 print("HERE")
 
